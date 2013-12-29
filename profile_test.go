8f0d31bd6716a5566063f8bbd2948a15fead804b
Wilhelm Murdoch <wilhelm.murdoch@gmail.com>
2013-12-30 00:29:28 +1100

    test fixed

--- profile_test.go
+++ profile_test.go
@@ -3,7 +3,6 @@ package biscuit
 import (
 	. "github.com/smartystreets/goconvey/convey"
 	"io/ioutil"
-	"log"
 	"path/filepath"
 	"testing"
 )
@@ -60,15 +59,11 @@ func TestProfile(t *testing.T) {
 				profiles[label] = NewProfileFromText(label, corpora[label], n)
 			}
 
-			unknowns := make(map[string]string)
+			profileInstances := make([]*Profile, 0, len(profiles))
 
-			unknowns["de"] = "Der Kanalinspektor natrlich!"
-			unknowns["en"] = "His listeners knew that he was going to say this."
-			// unknowns["es"] = "con sus aperos formados con prendas de procedencia."
-			unknowns["es"] = "why, hello there, good sir. Fine day we're having!"
-			unknowns["fr"] = "Monsieur le baron était un des plus puissants."
-			unknowns["jp"] = "っともすずしく、さらになんの奇跡か、季節はずれというのにまだイチゴが食"
-			unknowns["th"] = "ผู้ฟัง ของเขา รู้ว่า เขากำลังจะ พูดแบบนี้ พวกเขารู้มาก"
+			for _, profile := range profiles {
+				profileInstances = append(profileInstances, profile)
+			}
 
 			Convey("Comparing a corpus against itself should yield an exact match", func() {
 				for _, profile := range profiles {
@@ -92,19 +87,30 @@ func TestProfile(t *testing.T) {
 				}
 			})
 
-			profilesArray := make([]*Profile, 0, len(profiles))
+			Convey("DE sample text should score as DE", func() {
+				unknown := NewProfileFromText("unknown", "Der Kanalinspektor natrlich!", n)
+				So(unknown.Match(profileInstances), ShouldEqual, "de")
+			})
 
-			for _, p := range profiles {
-				profilesArray = append(profilesArray, p)
-			}
+			Convey("ES sample text should score as ES", func() {
+				unknown := NewProfileFromText("unknown", "con sus aperos formados con prendas de procedencia.", n)
+				So(unknown.Match(profileInstances), ShouldEqual, "es")
+			})
 
-			for _, profile := range profiles {
-				Convey(profile.Label+" sample text should score as "+profile.Label, func() {
-					log.Print(profile.Label)
-					unknown := NewProfileFromText("unknown", unknowns[profile.Label], n)
-					So(unknown.Match(profilesArray), ShouldEqual, profile.Label)
-				})
-			}
+			Convey("FR sample text should score as FR", func() {
+				unknown := NewProfileFromText("unknown", "Monsieur le baron était un des plus puissants.", n)
+				So(unknown.Match(profileInstances), ShouldEqual, "fr")
+			})
+
+			Convey("JP sample text should score as JP", func() {
+				unknown := NewProfileFromText("unknown", "っともすずしく、さらになんの奇跡か、季節はずれというのにまだイチゴが食", n)
+				So(unknown.Match(profileInstances), ShouldEqual, "jp")
+			})
+
+			Convey("TH sample text should score as TH", func() {
+				unknown := NewProfileFromText("unknown", "ผู้ฟัง ของเขา รู้ว่า เขากำลังจะ พูดแบบนี้ พวกเขารู้มาก", n)
+				So(unknown.Match(profileInstances), ShouldEqual, "th")
+			})
 		})
 	})
 }
